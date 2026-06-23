4688def0096efb8bf46b448500d7e6d6c4806308
Kornel Hajto <kornelhajto2004@gmail.com>
2026-06-23 14:28:27 +0200

    Mostly done formatting of the DVD text

--- main.c
+++ main.c
@@ -8,10 +8,10 @@
 #define P printf("\033[2J\033[%d;%dH[DVD]",y,x)
 #define B(p,m,d) if(p+d>=m||p+d<2)d=-d
 
-int main(){/*D*/        /*V*/         /*V*/   struct/* D  */
-winsize     /* */w       ;int        x=2      ,y=2,    a/* */=
-1,b=1;       /* */        while     (1)       {ioctl     /* */
-(1,T,&w)     /* */         ;P;     fflush     (0);       /* */
-usleep(     /* */           100000);B         (x,W,a)   /* */
-;B(y,H,b);/*D */             ;x+=a;y+=b;      }return  (0);}
-/*DVD*/ /*DVD*/               /*V*/           /*V*/ /*DVD*/
+int main(){ /*D*/    /*V*/         /*V*/   struct/* DVD */
+winsize      /* */w   ;int        x=2      ,y=2,      a/* */=
+1,b=1;        /* */    while     (1)       {ioctl      /* */
+(1,T,&w)      /* */     ;P;     fflush     (0);        /* */
+usleep(      /* */       100000);B         (x,W,a)    /* */
+;B(y,H,b); /*D */         ;x+=a;y+=b;      }return   (0);}
+/*DVD*/ /*DVD*/            /*V*/           /*V*/ /*DVD*/
